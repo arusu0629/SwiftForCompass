734458a51c8a79070078ddc4fe065eb4f90f2c3e
Toru_Nakandakari <arusu0629@gmail.com>
2020-03-16 13:02:13 +0900

    イベント取得のAPIを実装

---
+++ SwiftForCompass/SwiftForCompass/Agent.swift
@@ -0,0 +1,78 @@
+//
+//  Agent.swift
+//  SwiftForCompass
+//
+//  Created by Toru Nakandakari on 2020/03/16.
+//  Copyright © 2020 仲村渠亨. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+struct Agent {
+    struct Response<T> {
+        let value: T
+        let response: URLResponse
+    }
+    
+    enum CompassApiError: Error {
+        case network
+        case cannotCastHttpResponse
+        case cannotDecode
+        case unknown
+    }
+    
+    func run<T: Decodable>(_ request: URLRequest, onDecode: @escaping (Result<Response<T>, CompassApiError>) -> Void) {
+        let task = URLSession.shared.dataTask(with: request) { (data, urlResponse, error) in
+            func execCompletionOnMainThread(_ result: Result<Response<T>, CompassApiError>) {
+                DispatchQueue.main.async {
+                    onDecode(result)
+                }
+            }
+            
+            if error != nil {
+                execCompletionOnMainThread(.failure(.network))
+                return
+            }
+            
+            guard let httpResponse = urlResponse as? HTTPURLResponse else {
+                execCompletionOnMainThread(.failure(.cannotCastHttpResponse))
+                return
+            }
+            
+            switch httpResponse.statusCode {
+            case 200, 201, 204:
+                do {
+                    let jsonDecoder = JSONDecoder()
+                    let value = try jsonDecoder.decode(T.self, from: data ?? Data())
+                    let response = Response(value: value, response: httpResponse)
+                    execCompletionOnMainThread(.success(response))
+                } catch {
+                    execCompletionOnMainThread(.failure(.cannotDecode))
+                }
+            default:
+                execCompletionOnMainThread(.failure(.unknown))
+            }
+        }
+        
+        task.resume()
+    }
+}
+
+enum CompassApi {
+    static let agent = Agent()
+    static let base = URL(string: "https://connpass.com/api/v1")!
+}
+
+extension CompassApi {
+    static func searchEvent(_ withKeyword: String = "", completion: @escaping ((Result<Agent.Response<SearchEventsResponse>, Agent.CompassApiError>) -> Void)) {
+        let path = "event"
+        let url = base.appendingPathComponent(path)
+        var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true)!
+        urlComponents.queryItems = [
+            URLQueryItem(name: "keyword", value: withKeyword)
+        ]
+        let request = URLRequest(url: urlComponents.url!)
+        agent.run(request, onDecode: completion)
+    }
+}
---
+++ SwiftForCompass/SwiftForCompass/Entity/Event.swift
@@ -0,0 +1,57 @@
+//
+//  Event.swift
+//  SwiftForCompass
+//
+//  Created by Toru Nakandakari on 2020/03/16.
+//  Copyright © 2020 仲村渠亨. All rights reserved.
+//
+
+import Foundation
+
+struct SearchEventsResponse: Codable {
+    let resultReturned: Int // 含まれる検索結果の件数
+    let resultAvailable: Int // 検索結果の総件数
+    let events: [Event] // イベントリスト
+}
+
+extension SearchEventsResponse {
+    enum CodingKeys: String, CodingKey {
+        case resultReturned = "results_returned"
+        case resultAvailable = "results_available"
+        case events = "events"
+    }
+}
+
+struct Event: Codable {
+    let id: Int
+    let title: String
+    let description: String
+    let url: String
+    let startAt: String
+    let endAt: String
+    let address: String
+    let place: String
+    let lat: String?
+    let lon: String?
+    let limit: Int
+    let accepted: Int
+    let waiting: Int
+}
+
+extension Event {
+    enum CodingKeys: String, CodingKey {
+        case id = "event_id"
+        case title = "title"
+        case description = "description"
+        case url = "event_url"
+        case startAt = "started_at"
+        case endAt = "ended_at"
+        case address = "address"
+        case place = "place"
+        case lat = "lat"
+        case lon = "lon"
+        case limit = "limit"
+        case accepted = "accepted"
+        case waiting = "waiting"
+    }
+}
