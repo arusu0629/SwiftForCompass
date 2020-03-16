070913863f1e05a286ea35a513511243f433f8e6
Toru_Nakandakari <arusu0629@gmail.com>
2020-03-16 11:52:14 +0900

    add initial project

---
+++ SwiftForCompass/SwiftForCompass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftForCompass
+//
+//  Created by Toru Nakandakari on 2020/03/16.
+//  Copyright © 2020 仲村渠亨. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
